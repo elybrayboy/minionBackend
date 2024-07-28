915fb1f80d09ef89b551a94686b7e8c13955bb70
elybrayboy <117125005+elybrayboy@users.noreply.github.com>
2024-07-28 12:54:32 -0400

    attempt to fix broken links

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
